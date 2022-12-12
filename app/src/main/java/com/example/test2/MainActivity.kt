fcae1767ca147c41f5060daf0cdc80f9ce2d754c
danii <daniil82484@gmail.com>
2022-12-13 00:26:59 +0500

    nav3

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.CountDownTimer
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val timer = object : CountDownTimer(4000,1000){
